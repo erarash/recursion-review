628557a062958a2035be1aca3b8b571bcf578923
erarash <erani.arash@gmail.com>
2018-09-17 11:45:57 -0700

    commiting getElementByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,33 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+
+// var htmlStrings = [
+//   '<div class="targetClassName"></div>',
+//   '<div class="otherClassName targetClassName"></div>',
+//   '<div><div class="targetClassName"></div></div>',
+//   '<div><div class="targetClassName"><div class="targetClassName"></div></div></div>',
+//   '<div><div></div><div><div class="targetClassName"></div></div></div>',
+//   '<div><div class="targetClassName"></div><div class="targetClassName"></div></div>',
+//   '<div><div class="somediv"><div class="innerdiv"><span class="targetClassName">yay</span></div></div></div>'
+// ];
+
+var getElementsByClassName = function(className) {
+  var result =  [];
+  
+  var findClassName = function(node) {
+   if (node.className && node.classList.contains(className)) {
+     result.push(node); 
+   }
+   
+   if (node.childNodes) {
+     for (var item of node.childNodes) {
+       findClassName(item);      
+     }
+   }
+  }
+  findClassName(document.body);
+  return result;
+ //console.log(result), why doesn't it print anything?
 };
+
